09000d8a8d24f7bc069c02f21a2c881fa025ad15
junichi11 <junichi0206@gmail.com>
2020-07-02 13:10:21 +0900

    Improve ImageCompletionProvider

--- src/com/junichi11/netbeans/modules/html/enhancements/editor/ImageCompletionProvider.java
+++ src/com/junichi11/netbeans/modules/html/enhancements/editor/ImageCompletionProvider.java
@@ -15,17 +15,15 @@
  */
 package com.junichi11.netbeans.modules.html.enhancements.editor;
 
+import com.junichi11.netbeans.modules.html.enhancements.utils.DocUtils;
 import java.awt.Image;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.annotations.common.CheckForNull;
+import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.api.editor.mimelookup.MimeRegistrations;
 import org.netbeans.api.html.lexer.HTMLTokenId;
@@ -33,23 +31,20 @@ import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.api.lexer.TokenUtilities;
-import org.netbeans.modules.csl.spi.GsfUtilities;
 import org.netbeans.modules.php.editor.lexer.PHPTokenId;
 import org.netbeans.spi.editor.completion.CompletionProvider;
 import org.netbeans.spi.editor.completion.CompletionResultSet;
 import org.netbeans.spi.editor.completion.CompletionTask;
 import org.netbeans.spi.editor.completion.support.AsyncCompletionQuery;
 import org.netbeans.spi.editor.completion.support.AsyncCompletionTask;
-import org.openide.filesystems.FileObject;
-import org.openide.util.Exceptions;
 
 /**
  *
  * @author junichi11
  */
 @MimeRegistrations({
-    @MimeRegistration(mimeType = "text/html", service = CompletionProvider.class),
-    @MimeRegistration(mimeType = "text/x-php5", service = CompletionProvider.class)
+    @MimeRegistration(mimeType = DocUtils.HTML_MIME_TYPE, service = CompletionProvider.class),
+    @MimeRegistration(mimeType = DocUtils.PHP_MIME_TYPE, service = CompletionProvider.class)
 })
 public class ImageCompletionProvider implements CompletionProvider {
 
@@ -60,103 +55,7 @@ public class ImageCompletionProvider implements CompletionProvider {
         if (queryType != CompletionProvider.COMPLETION_QUERY_TYPE) {
             return null;
         }
-        return new AsyncCompletionTask(new AsyncCompletionQuery() {
-            @Override
-            protected void query(CompletionResultSet resultSet, Document doc, int caretOffset) {
-                AbstractDocument ad = (AbstractDocument) doc;
-                ad.readLock();
-                try {
-                    // get token sequence
-                    TokenSequence<HTMLTokenId> ts = getTokenSequence(doc, caretOffset);
-                    if (ts == null) {
-                        return;
-                    }
-                    ts.move(caretOffset);
-                    ts.moveNext(); // current
-                    ts.movePrevious(); // =?
-                    ts.movePrevious(); // attribute?
-
-                    // check whether attribute is width or height
-                    Token token = ts.token();
-                    CharSequence tokenText = token.text();
-                    boolean isWidth = isWidth(tokenText);
-                    boolean isHeight = isHeight(tokenText);
-                    if (token.id() != HTMLTokenId.ARGUMENT) {
-                        return;
-                    }
-                    if (!isWidth && !isHeight) {
-                        return;
-                    }
-
-                    // search "src"
-                    while (ts.movePrevious()) {
-                        Token t = ts.token();
-                        if (TokenUtilities.equals(t.text(), "src")) { // NOI18N
-                            break;
-                        }
-                        if (t.id() == HTMLTokenId.TAG_OPEN) {
-                            return;
-                        }
-                    }
-                    ts.moveNext();
-                    ts.moveNext();
-                    token = ts.token();
-                    CharSequence text = token.text();
-                    if (TokenUtilities.equals(text, "\"\"")) { // NOI18N
-                        return;
-                    }
-
-                    // img src
-                    String imagePath = normalizeImagePath(text.toString());
-                    Image image = null;
-
-                    // URL
-                    if (imagePath.startsWith("http://") || imagePath.startsWith("https://")) { // NOI18N
-                        try {
-                            URL imageUrl = new URL(imagePath);
-                            image = ImageIO.read(imageUrl);
-                        } catch (MalformedURLException ex) {
-                            LOGGER.log(Level.WARNING, null, ex);
-                            return;
-                        } catch (IOException ex) {
-                            LOGGER.log(Level.WARNING, null, ex);
-                            return;
-                        }
-                    }
-
-                    // Path
-                    FileObject currentFile = getFileObject(doc);
-                    if (currentFile != null && image == null && !imagePath.isEmpty()) {
-                        FileObject imageFile = currentFile.getFileObject(imagePath);
-                        if (imageFile == null) {
-                            return;
-                        }
-                        try {
-                            image = ImageIO.read(imageFile.getInputStream());
-                        } catch (IOException ex) {
-                            Exceptions.printStackTrace(ex);
-                        }
-                    }
-                    if (image == null) {
-                        return;
-                    }
-
-                    // get value of width or height
-                    int value = 0;
-                    if (isHeight) {
-                        value = image.getHeight(null);
-                    } else if (isWidth) {
-                        value = image.getWidth(null);
-                    } else {
-                        // bug
-                    }
-                    resultSet.addItem(new HtmlEnhancementCompletionItem(String.valueOf(value), caretOffset, 0));
-                } finally {
-                    ad.readUnlock();
-                    resultSet.finish();
-                }
-            }
-        }, component);
+        return new AsyncCompletionTask(new AsyncCompletionQueryImpl(), component);
     }
 
     @Override
@@ -172,8 +71,8 @@ public class ImageCompletionProvider implements CompletionProvider {
      * @return
      */
     @SuppressWarnings("unchecked")
-    protected TokenSequence<HTMLTokenId> getTokenSequence(Document doc, int caretOffset) {
-        TokenHierarchy hierarchy = TokenHierarchy.get(doc);
+    private static TokenSequence<HTMLTokenId> getTokenSequence(Document doc, int caretOffset) {
+        TokenHierarchy<Document> hierarchy = TokenHierarchy.get(doc);
         TokenSequence<HTMLTokenId> ts = hierarchy.tokenSequence(HTMLTokenId.language());
         if (ts == null) {
             TokenSequence<PHPTokenId> phpTs = hierarchy.tokenSequence(PHPTokenId.language());
@@ -187,34 +86,12 @@ public class ImageCompletionProvider implements CompletionProvider {
     }
 
     /**
-     * Create path For FileObject
-     *
-     * @param imagePath
-     * @return normalized path
-     */
-    protected String normalizeImagePath(String imagePath) {
-        imagePath = imagePath.substring(1, imagePath.length() - 1);
-        if (imagePath.startsWith("./")) { // NOI18N
-            imagePath = "." + imagePath; // NOI18N
-        } else if (imagePath.startsWith("https://")) { // NOI18N
-            // do nothing
-        } else if (imagePath.startsWith("http://")) { // NOI18N
-            // do nothing
-        } else if (imagePath.isEmpty()) {
-            // do nothing
-        } else {
-            imagePath = "../" + imagePath; // NOI18N
-        }
-        return imagePath;
-    }
-
-    /**
      * Check whether attribute is width.
      *
      * @param text token text
      * @return true if attribute is width, otherwise false.
      */
-    protected boolean isWidth(CharSequence text) {
+    private static boolean isWidth(CharSequence text) {
         return TokenUtilities.equals(text, "width"); // NOI18N
     }
 
@@ -224,18 +101,127 @@ public class ImageCompletionProvider implements CompletionProvider {
      * @param text token text
      * @return true if attribute is height, otherwise false.
      */
-    protected boolean isHeight(CharSequence text) {
+    private static boolean isHeight(CharSequence text) {
         return TokenUtilities.equals(text, "height"); // NOI18N
     }
 
-    /**
-     * Get FileObject with Document.
-     *
-     * @param doc Document
-     * @return FileObject
-     */
-    @CheckForNull
-    private FileObject getFileObject(Document doc) {
-        return GsfUtilities.findFileObject(doc);
+    //~Inner classes
+    private static class AsyncCompletionQueryImpl extends AsyncCompletionQuery {
+
+        public AsyncCompletionQueryImpl() {
+        }
+
+        @Override
+        protected void query(CompletionResultSet resultSet, Document doc, int caretOffset) {
+            AbstractDocument ad = (AbstractDocument) doc;
+            ad.readLock();
+            try {
+                // get token sequence
+                TokenSequence<HTMLTokenId> ts = getTokenSequence(doc, caretOffset);
+                if (ts == null) {
+                    return;
+                }
+
+                // check whether attribute is width or height
+                ImgAttribute imgAttr = createImgAttribute(ts, caretOffset);
+                if (imgAttr == ImgAttribute.NONE) {
+                    return;
+                }
+
+                String imgPath = getImgSrcPath(ts);
+                if (imgPath == null) {
+                    return;
+                }
+                Image image = DocUtils.getImage(imgPath, doc);
+                if (image == null) {
+                    return;
+                }
+                resultSet.addItem(new HtmlEnhancementCompletionItem(getImageSize(image, imgAttr), caretOffset, 0));
+            } finally {
+                ad.readUnlock();
+                resultSet.finish();
+            }
+        }
+
+        @NonNull
+        private ImgAttribute createImgAttribute(TokenSequence<HTMLTokenId> ts, int caretOffset) {
+            ts.move(caretOffset);
+            ts.moveNext(); // current
+            if (ts.offset() == caretOffset) { // ^""
+                return ImgAttribute.NONE;
+            }
+            ts.movePrevious(); // =?
+            ts.movePrevious(); // attribute?
+            Token<HTMLTokenId> token = ts.token();
+            if (token == null
+                    || token.id() != HTMLTokenId.ARGUMENT) {
+                return ImgAttribute.NONE;
+            }
+            CharSequence tokenText = token.text();
+            return ImgAttribute.create(isHeight(tokenText), isWidth(tokenText));
+        }
+
+        @CheckForNull
+        private String getImgSrcPath(TokenSequence<HTMLTokenId> ts) {
+            while (ts.movePrevious()) {
+                Token<HTMLTokenId> token = ts.token();
+                if (TokenUtilities.equals(token.text(), "src")) { // NOI18N
+                    break;
+                }
+                if (token.id() == HTMLTokenId.TAG_OPEN) {
+                    return null;
+                }
+            }
+            ts.moveNext();
+            ts.moveNext();
+            Token<HTMLTokenId> token = ts.token();
+            CharSequence srcText = token.text();
+            if (srcText == null || TokenUtilities.equals(srcText, "\"\"")) { // NOI18N
+                return null;
+            }
+            String imgPath = srcText.toString();
+            return imgPath.substring(1, imgPath.length() - 1);
+        }
+
+        private String getImageSize(Image image, ImgAttribute imgAttribute) {
+            // get value of width or height
+            int value = 0;
+            if (imgAttribute.isHeight()) {
+                value = image.getHeight(null);
+            } else if (imgAttribute.isWidth()) {
+                value = image.getWidth(null);
+            } else {
+                LOGGER.log(Level.WARNING, "Not hight and width"); // NOI18N
+            }
+            return String.valueOf(value);
+        }
+
+    }
+
+    private static final class ImgAttribute {
+
+        private final boolean isHeight;
+        private final boolean isWidth;
+        private static final ImgAttribute NONE = new ImgAttribute(false, false);
+
+        private ImgAttribute(boolean isHeight, boolean isWidth) {
+            this.isHeight = isHeight;
+            this.isWidth = isWidth;
+        }
+
+        public static ImgAttribute create(boolean isHeight, boolean isWidth) {
+            if (!isHeight && !isWidth) {
+                return NONE;
+            }
+            return new ImgAttribute(isHeight, isWidth);
+        }
+
+        public boolean isHeight() {
+            return isHeight;
+        }
+
+        public boolean isWidth() {
+            return isWidth;
+        }
     }
 }
